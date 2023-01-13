aa87bf7ec7ac5ed5dd42c327487cbd700656539e
FlavioSouzaSantos <desouza.flavio@gmail.com>
2023-01-12 23:17:15 -0400

    Criando o projeto Config Server e realizando as devidas configurações nos demais projetos que vai utilziar ele.

---
+++ config-server/src/main/java/br/com/mymoney/configserver/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package br.com.mymoney.configserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Value("${spring.security.user.name:admin}")
+    private String username;
+
+    @Value("${spring.security.user.password:admin}")
+    private String password;
+
+    @Value("${spring.security.user.role:ADMIN}")
+    private String role;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().requestMatchers("/**").hasRole(role).and().httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles(role)
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
